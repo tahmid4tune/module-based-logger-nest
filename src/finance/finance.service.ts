e6a3801a0e31db527f65c2c3d17bc4c36a65b1b7
tahmid4tune <tahmid.tee10@gmail.com>
2024-10-13 11:15:51 +0600

    .added

--- src/finance/finance.service.ts
+++ src/finance/finance.service.ts
@@ -1,14 +1,16 @@
-import { Injectable } from '@nestjs/common';
-import { LoggerService } from 'src/logger/logger.service';
+import { Injectable } from "@nestjs/common";
+import { LoggerService } from "src/logger/logger.service";
 
 @Injectable()
 export class FinanceService {
   constructor(private readonly loggerService: LoggerService) {}
 
   logTest() {
-    this.loggerService.log("Finance log goes to the finance folder's log file");
+    this.loggerService.log(
+      "Finance log goes to the finance folder's log file."
+    );
     this.loggerService.error(
-      "Finance error goes to the finance folder's error file",
+      "Finance error goes to the finance folder's error file."
     );
   }
 }
